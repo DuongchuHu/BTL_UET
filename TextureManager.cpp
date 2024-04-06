9238f9b6229c316de650eadc9b83145d5bbda80a
DuongchuHu <vinh1896saovang@gmail.com>
2024-04-06 16:46:17 +0700

    Update TextureManager.cpp

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -10,4 +10,36 @@ SDL_Texture *TextureManager::Loadtexture(const char *texture)
 
     return tex;
 }
+SDL_Texture* TextureManager::LoadFontTexture(const char* text, int size){
+    TTF_Font* Font = TTF_OpenFont("LIT-Mango.ttf", size);
+
+    SDL_Color textColor = { 255, 255, 255 };
+    SDL_Surface* textSurface = TTF_RenderText_Solid(Font, text, textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(Game::renderer, textSurface);
+    SDL_FreeSurface(textSurface);
+
+    return textTexture;
+}
+#endif#ifndef TextureManager_HPP
+#define TextureManager_HPP
+#include "TextureManager.hpp"
+
+SDL_Texture *TextureManager::Loadtexture(const char *texture)
+{
+    SDL_Surface* tmpSurface = IMG_Load(texture);
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tmpSurface);
+    SDL_FreeSurface(tmpSurface);
+
+    return tex;
+}
+SDL_Texture* TextureManager::LoadFontTexture(const char* text, int size){
+    TTF_Font* Font = TTF_OpenFont("LIT-Mango.ttf", size);
+
+    SDL_Color textColor = { 255, 255, 255 };
+    SDL_Surface* textSurface = TTF_RenderText_Solid(Font, text, textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(Game::renderer, textSurface);
+    SDL_FreeSurface(textSurface);
+
+    return textTexture;
+}
 #endif
