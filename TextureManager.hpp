56d28cbc817a93bee2813c35b5bb19b2c2fd9efb
DuongchuHu <vinh1896saovang@gmail.com>
2024-04-09 00:20:07 +0700

    Update TextureManager.hpp

--- TextureManager.hpp
+++ TextureManager.hpp
@@ -8,5 +8,5 @@ public:
     TextureManager();
     ~TextureManager();
     static SDL_Texture* Loadtexture(const char* fileName);
-    static SDL_Texture* LoadFontTexture(const char* text, int size);
+    static SDL_Texture* LoadFontTexture(const char* text, int size, const char* Choose_font, const char* color);
 };
